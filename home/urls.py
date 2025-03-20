9220e598f4120b6bf9fcbe4c9a9dd7b9cfbfe27f
Tushar Patil <71126405+tushaaaarr@users.noreply.github.com>
2025-03-20 18:56:40 +0530

    added order details

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("packages/", views.get_packages, name="get_packages"),
     path('create-order/', userinfo_with_orders, name='userinfo_with_orders'),
-    path('orders/<str:order_id>/', views.get_order_details, name='get_order_details'), 
+    path('orders/', views.get_order_details, name='get_order_details'), 
 
 
     path('lky8/webhook/check-payment-status/', views.payment_webhook, name='payment_webhook'),
